f29417f7a046ccd14d0156dd5a86a0372affc79c
litao1104 <litao1104@gmail.com>
2019-12-01 23:53:22 +0800

    update: acc result code notify

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,22 +23,22 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.add_item:
-            Toast.makeText(this, "You clicked Add",
-                    Toast.LENGTH_SHORT).show();
-            break;
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add",
+                        Toast.LENGTH_SHORT).show();
+                break;
 
-        case R.id.remove_item:
-            Toast.makeText(this, "You clicked Remove",
-                    Toast.LENGTH_SHORT).show();
-            break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove",
+                        Toast.LENGTH_SHORT).show();
+                break;
 
-        case R.id.exit_item:
-            finish();
-            break;
+            case R.id.exit_item:
+                finish();
+                break;
 
-        default:
-            break;
+            default:
+                break;
         }
 
         return true;
@@ -57,10 +58,30 @@ public class FirstActivity extends AppCompatActivity {
                  *       Toast.LENGTH_SHORT).show();
                  */
 
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.baidu.com"));
-                startActivity(intent);
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setData(Uri.parse("http://www.baidu.com"));
+                //startActivity(intent);
+
+                //String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //intent.putExtra("extra_data", data);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -2,13 +2,37 @@ package com.example.activitytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello FirstActivity");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+
+        Button button2 = (Button) findViewById(R.id.buttion_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
